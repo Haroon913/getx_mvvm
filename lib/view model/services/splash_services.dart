59afa406fd1b79ab0a177014513ab306954d6ecc
Haroon <rajaharoon973@gmail.com>
2024-09-13 18:25:53 +0500

    getx

--- lib/view model/services/splash_services.dart
+++ lib/view model/services/splash_services.dart
@@ -1,9 +1,7 @@
 import 'package:get/get.dart';
 import 'package:getx_mvvm/res/routes/routes_name.dart';
 import 'dart:async';
-
 import 'package:getx_mvvm/view%20model/controller/user_preferences/user_preferences_view_model.dart';
-
 import '../../utils/utils.dart';
 
 class SplashServices{
